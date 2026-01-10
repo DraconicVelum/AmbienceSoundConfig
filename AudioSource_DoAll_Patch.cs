d470f9206fd812f91cc17a4866bbd5e53fbfc958
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-01-10 00:47:36 +0000

    Add Harmony patches for AudioSource methods

---
+++ AudioSource_DoAll_Patch.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace AmbienceSoundConfig
+{
+    [HarmonyPatch(typeof(AudioSource))]
+    internal static class AudioSource_DoAll_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(AudioSource.Play), new System.Type[0])]
+        private static void Postfix_Play(AudioSource __instance)
+        {
+            AudioSourceFilter.Apply(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(AudioSource.Play), new[] { typeof(ulong) })]
+        private static void Postfix_PlayDelayed(AudioSource __instance)
+        {
+            AudioSourceFilter.Apply(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(AudioSource.PlayOneShot), new[] { typeof(AudioClip) })]
+        private static void Postfix_PlayOneShot(AudioSource __instance)
+        {
+            AudioSourceFilter.Apply(__instance);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("set_volume")]
+        private static void Prefix_SetVolume(AudioSource __instance, ref float value)
+        {
+            AudioSourceFilter.Apply(__instance, ref value);
+        }
+    }
+}
