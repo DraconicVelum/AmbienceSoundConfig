bcc3c143932939c2eba7445dfa953d205199621d
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-02-05 23:51:29 +0000

    Update AmbienceSoundConfig to version 2.6.8
    Added new configuration options for sound logging and clip volume.

--- AmbienceSoundConfig.cs
+++ AmbienceSoundConfig.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace AmbienceSoundConfig
 {
-    [BepInPlugin("com.draconicvelum.ambiencesoundconfig", "Ambience Sound Config", "2.5.0")]
+    [BepInPlugin("com.draconicvelum.ambiencesoundconfig", "Ambience Sound Config", "2.6.8")]
     public class AmbienceSoundConfig : BaseUnityPlugin
     {
         public static ConfigEntry<float> MasterVolume;
@@ -16,6 +16,11 @@ namespace AmbienceSoundConfig
         public static ConfigEntry<float> ShieldHumVolume;
         public static ConfigEntry<string> ExtraSfxList;
         public static ConfigEntry<float> ExtraSfxVolume;
+        public static ConfigEntry<string> ExtraClipList;
+        public static ConfigEntry<float> ExtraClipVolume;
+        public static ConfigEntry<bool> EnableSoundLogging;
+        public static ConfigEntry<bool> LogOnlyUnique;
+
 
         private static readonly Harmony harmony = new Harmony("com.draconicvelum.ambiencesoundconfig");
 
@@ -52,26 +57,71 @@ namespace AmbienceSoundConfig
                     new AcceptableValueRange<float>(0f, 1f)
                 )
             );
+            ExtraClipList = Config.Bind(
+                "Extra Clips",
+                "Extra Clip Names",
+                "",
+                "Comma separated list of AudioClip names to mute or scale."
+            );
+
+            ExtraClipVolume = Config.Bind(
+                "Extra Clips",
+                "Extra Clip Volume",
+                1.0f,
+                new ConfigDescription(
+                    "Independent volume multiplier for listed clip names.",
+                    new AcceptableValueRange<float>(0f, 1f)
+                )
+            );
+
+            EnableSoundLogging = Config.Bind(
+                "Logging",
+                "Enable Sound Logging",
+                false,
+                "Logs all played SFX and AudioClips to console and a file next to this config."
+            );
+
+            LogOnlyUnique = Config.Bind(
+                "Logging",
+                "Log Only Unique",
+                true,
+                "If enabled, each sound name is logged only once per session."
+            );
+
             ExtraSfxList.SettingChanged += (_, __) => AudioSourceFilter.Refresh();
             ExtraSfxVolume.SettingChanged += (_, __) =>
             {
                 foreach (var src in UnityEngine.Object.FindObjectsOfType<AudioSource>())
                     AudioSourceFilter.Apply(src);
             };
+
+            ExtraClipList.SettingChanged += (_, __) =>
+            {
+                AudioSourceFilter.Refresh();
+            };
+
+            ExtraClipVolume.SettingChanged += (_, __) =>
+            {};
+
             AudioSourceFilter.Refresh();
+            SoundPlayLogger.Init(Config);
 
             harmony.PatchAll(typeof(AudioSource_DoAll_Patch));
-
+            harmony.PatchAll(typeof(AudioSource_PlayOneShotScale_Patch));
+            harmony.PatchAll(typeof(ZSFX_VolumeScale_Patch));
             harmony.PatchAll(typeof(AudioMan_AmbienceVolume_Patch));
             harmony.PatchAll(typeof(SettingsInject));
 
             Config.ConfigReloaded += OnReloaded;
+            Logger.LogInfo("Ambience Sound Config (v2.6.8) loaded.");
 
-            Logger.LogInfo("Ambience Sound Config (v2.5.0) loaded.");
         }
 
         private void OnReloaded(object sender, EventArgs e)
         {
+            AudioSourceFilter.Refresh();
+            AudioSourceFilter.ApplyToRunningSources();
+
             ApplyAllVolumes();
             SettingsInject.SyncUI();
         }
