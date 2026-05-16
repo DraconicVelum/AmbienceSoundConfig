a197acf946f0e7f332074664017fc19273631515
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-05-16 13:22:40 +0100

    Add files via upload

---
+++ AudioSource_PlayOneShotScale_Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace AmbienceSoundConfig
+{
+    [HarmonyPatch(typeof(AudioSource))]
+    internal static class AudioSource_PlayOneShotScale_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(AudioSource.PlayOneShot), new[] { typeof(AudioClip), typeof(float) })]
+        private static void Prefix(AudioSource __instance, AudioClip clip, ref float volumeScale)
+        {
+            SoundPlayLogger.Log("CLIP", clip?.name, __instance ? __instance.gameObject : null);
+
+            if (clip == null)
+                return;
+
+            if (!AudioSourceFilter.HasClipTargets)
+                return;
+
+            if (!AudioSourceFilter.TryGetClipMultiplier(clip, out float mult))
+                return;
+
+            volumeScale = mult <= 0f ? 0f : volumeScale * mult;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(AudioSource.PlayOneShot), new[] { typeof(AudioClip) })]
+        private static void Prefix_NoScale(AudioSource __instance, AudioClip clip)
+        {
+            SoundPlayLogger.Log("CLIP", clip?.name, __instance ? __instance.gameObject : null);
+        }
+    }
+}
